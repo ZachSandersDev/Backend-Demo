83692f0799640b00026be70458b3f1602c206f85
Zach S <zachzachzach2@gmail.com>
2022-12-09 08:12:58 -0800

    Fix type import and remove comments

--- src/__tests__/endpoints/pullRequestSummary.test.ts
+++ src/__tests__/endpoints/pullRequestSummary.test.ts
@@ -1,37 +1,7 @@
-// import { Application, Request, Response } from "express";
-
 import { handlePullRequestSummary } from "../../endpoints/pullRequestSummary";
 
 import * as pullSummaryMod from "../../github/getPullRequestSummary";
-
-// import getPullRequestSummary from "../github/getPullRequestSummary";
-
-// export function registerPullRequestEndpoints(app: Application): void {
-//   app.get("/pulls/summary/:repoURL", handlePullRequestSummary);
-// }
-
-// export async function handlePullRequestSummary(
-//   req: Request,
-//   res: Response
-// ): Promise<void> {
-//   const repoURL = new URL(req.params.repoURL);
-
-//   // Remove the leading slash and split the url to get the owner and repo
-//   const [owner, repo] = repoURL.pathname.slice(1).split("/");
-
-//   try {
-//     const summary = await getPullRequestSummary(owner, repo);
-//     res.json(summary);
-//   } catch (err) {
-//     res.status(400);
-
-//     if (err instanceof Error) {
-//       res.send(err?.message);
-//     } else {
-//       res.send(`Could not get pull request summary for ${repoURL.toString()}`);
-//     }
-//   }
-// }
+import { PullRequestSummary } from "../../github/githubTypes";
 
 describe("pullRequestSummaryEndpoint", () => {
   const mockReq = {
