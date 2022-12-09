a63d7ebc087b28e7aa010504537eefb96513ed34
Zach S <zachzachzach2@gmail.com>
2022-12-08 19:30:33 -0800

    Make some adjustments to appease the linter

--- src/endpoints/index.ts
+++ src/endpoints/index.ts
@@ -1,6 +1,6 @@
 import { Application } from "express";
-import { registerPullRequestEndpoints } from "./pullRequestSummary"
+import { registerPullRequestEndpoints } from "./pullRequestSummary";
 
-export default function registerAllEndpoints(app: Application) {
+export default function registerAllEndpoints(app: Application): void {
   registerPullRequestEndpoints(app);
 }
--- src/github/utils.ts
+++ src/github/utils.ts
@@ -1,17 +1,20 @@
-import fetch from "cross-fetch"
+import fetch from "cross-fetch";
 
-export function fetchWithGHCredentials(resource: RequestInfo, options?: RequestInit) {
+export async function fetchWithGHCredentials(
+  resource: RequestInfo,
+  options?: RequestInit
+): Promise<Response> {
   if (!process.env.GITHUB_ACCESS_TOKEN) {
-    throw "Github token not found";
+    throw new Error("Github token not found");
   }
 
-  return fetch(resource, {
+  return await fetch(resource, {
     ...options,
     headers: {
       ...options?.headers,
-      "Accept": "application/vnd.github+json",
-      "Authorization": `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
+      Accept: "application/vnd.github+json",
+      Authorization: `Bearer ${process.env.GITHUB_ACCESS_TOKEN}`,
       "X-GitHub-Api-Version": "2022-11-28",
-    }
-  })
+    },
+  });
 }
